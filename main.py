00c354bc3e2b55336ce3eca1d7878d76545cd25d
ardit <dpfrjyt@gmail.com>
2024-02-01 20:43:14 +0200

    small changes, add requirements.txt

--- main.py
+++ main.py
@@ -7,9 +7,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 
+class Categories(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    products = db.relationship('Products', backref='categories', lazy=True)
+
+
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    categories_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+
+
 @app.route('/')
 def home():
-
     return render_template('index.html')
 
 

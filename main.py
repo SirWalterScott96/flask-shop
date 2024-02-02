860ad5af6b792d56fcf81ec71e5864c052050d60
ardit <dpfrjyt@gmail.com>
2024-02-02 21:21:18 +0200

    write code related to admin route

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
-from flask_login import UserMixin, LoginManager, login_user, login_required, current_user
+from flask_migrate import Migrate
+from flask_login import UserMixin, LoginManager, login_user, login_required, current_user, logout_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
 import uuid
 
 
@@ -10,8 +14,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'lapo4kado4ka'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 login_manager = LoginManager(app)
-admin = Admin(app)
+login_manager.init_app(app)
 
 
 @login_manager.user_loader
@@ -21,6 +26,16 @@ def load_user(user_id):
             return Admin.query.get(user_id)
     except ValueError:
         return User.query.get(user_id)
+
+
+def only_admin_access(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if current_user.is_active and uuid.UUID(str(current_user.id)):
+            return func(*args, **kwargs)
+        else:
+            return redirect(url_for('admin_login'))
+    return wrapper
 
 
 class Categories(db.Model):
@@ -44,6 +59,9 @@ class Admin(db.Model, UserMixin):
     password = db.Column(db.String(100), nullable=False)
     role = db.Column(db.String(20))
 
+    def is_active(self):
+        return True
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -53,11 +71,41 @@ class User(db.Model, UserMixin):
     active = db.Column(db.Boolean(), default=True)
 
 
-admin.add_view(ModelView(Admin, db.session))
+class AdminLoginForm(FlaskForm):
+    name = StringField(validators=[DataRequired()])
+    password = PasswordField(validators=[DataRequired()])
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/admin')
+@only_admin_access
+def admin():
+    return render_template('admin.html')
+
+
+@app.route('/admin-login', methods=['GET', 'POST'])
+def admin_login():
+    form = AdminLoginForm()
+    if form.validate_on_submit():
+        admin_local = Admin.query.filter_by(name=form.name.data).first()
+        if not admin_local:
+            return redirect(url_for('admin-login'))
+
+        if check_password_hash(admin_local.password, form.password.data):
+            login_user(admin_local)
+            return redirect(url_for('admin'))
+
+    return render_template('admin_login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':

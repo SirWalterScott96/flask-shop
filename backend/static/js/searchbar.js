3a96897bc0e64e714c02b6f9322c373bff952cde
evgenkudrik <dpfrjyt@gmail.com>
2024-05-29 12:33:38 +0300

    small fixes in templates

--- backend/static/js/searchbar.js
+++ backend/static/js/searchbar.js
@@ -1,44 +1,46 @@
-$(document).ready(function (){
-    var allProducts = {};
+$(document).ready(function () {
+  var allProducts = {};
 
-    $.ajax({
-        type: 'GET',
-        url: '/product/get-all-products',
-        success: function(response) {
-            allProducts = response;
-        }
-    });
+  $.ajax({
+    type: "GET",
+    url: "/product/get-all-products",
+    success: function (response) {
+      allProducts = response;
+    },
+  });
 
-    const resultSearchBox = $('.search-result-box');
-    const inputBox = $('.input-box');
+  const resultSearchBox = $(".search-result-box");
+  const inputBox = $(".input-box");
 
-    inputBox.on('keyup', function (){
-        let result = [];
-        let input = inputBox.val().toLowerCase();
-        if(input.length > 2) {
-            result = Object.keys(allProducts).filter(function (key){
-               return key.toLowerCase().includes(input);
-            })
-        }
-        displayResultBox(result);
-    });
+  inputBox.on("keyup", function () {
+    let result = [];
+    let input = inputBox.val().toLowerCase();
+    if (input.length > 2) {
+      result = Object.keys(allProducts).filter(function (key) {
+        return key.toLowerCase().includes(input);
+      });
+    }
+    displayResultBox(result);
+  });
 
-    function displayResultBox(result){
+  function displayResultBox(result) {
     const content = result.map((list) => {
-        return $('<li>').text(list);
+      return $("<li>").text(list);
     });
 
     resultSearchBox.empty();
 
     if (content.length > 0) {
-        const ul = $('<ul>').append(content);
-        resultSearchBox.append(ul);
+      const ul = $("<ul>").append(content);
+      resultSearchBox.append(ul);
     }
-    }
+  }
 
-    resultSearchBox.on('click', 'li', function() {
-        var selectedProduct = $(this).text();
-        inputBox.val(selectedProduct);
-        resultSearchBox.empty();
-    });
+  resultSearchBox.on("click", "li", function () {
+    var selectedProduct = $(this).text();
+    inputBox.val(selectedProduct);
+    resultSearchBox.empty();
+    $(".search-btn").click();
+    console.log(1);
+  });
 });
